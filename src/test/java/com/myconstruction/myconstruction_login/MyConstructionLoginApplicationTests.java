813ae555ff86b0adc265d8b9fe47787ef9577b40
dpareja <daniel@dmtec.cl>
2025-11-17 21:57:39 -0300

    Entrega inicial: Aplicacion empresarial WAR

---
+++ src/test/java/com/myconstruction/myconstruction_login/MyConstructionLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myconstruction.myconstruction_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyConstructionLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
